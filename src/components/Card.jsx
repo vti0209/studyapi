d3650e8c675f6ba24191cbdb842b8f87f5830d16
vti0209 <hotrai84@gmail.com>
2026-05-12 08:56:05 +0700

    fix: display

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,8 @@ export function Card({ name, price, categoryName, image, index }) {
       <img src={image} className="card-img-top bg-white" alt={name} style={{ height: '200px', objectFit: 'contain', padding: '15px' }} />
       <div className="card-body text-center">
         <h5 className="card-title text-primary fw-bold">Sản phẩm {index + 1}: {name}</h5>
-        <h6 className="card-subtitle mb-2 text-muted fst-italic">Danh mục: {categoryName}</h6>
         <p className="card-text fw-bold text-danger mt-3 fs-5">Giá: ${price}</p>
+        <h6 className="card-subtitle mb-2 text-muted fst-italic">Danh mục: {categoryName}</h6>
       </div>
     </div>
   )
--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -5,7 +5,8 @@ export function ProductsList({ items }) {
     <div className="row">
       {items.map((item, index) => ( // index là chỉ số của phần tử trong mảng, dùng để hiển thị số thứ tự sản phẩm
         <div className="col-md-4 mb-4" key={item.id}> 
-          <Card {...item} index={index} /> // truyền tất cả thuộc tính của item vào Card component, cùng với index để hiển thị số thứ tự
+          <Card {...item} index={index} /> 
+          {/* truyền tất cả thuộc tính của item vào Card component, cùng với index để hiển thị số thứ tự */}
         </div>
       ))}
     </div>
